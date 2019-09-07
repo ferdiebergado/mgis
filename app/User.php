0e6dd6d843a00a081ff63ce2a0935ed2ea689073
ferdie <ferdiebergado@gmail.com>
2019-09-07 08:35:18 +0800

    Added fields to User Model

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role', 'active'
     ];
 
     /**
@@ -36,4 +36,14 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected $appends = ['hash'];
+
+    /**
+     * @return string
+     */
+    public function getHashAttribute()
+    {
+        return encrypt($this->id);
+    }
 }
